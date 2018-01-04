178c089d25aca1c2040b08c3bd0dc54bc7d856eb
Josh Trahan <josht0288@gmail.com>
2018-01-03 18:38:17 -0600

    sorta got it working

--- Driver.java
+++ Driver.java
@@ -15,30 +15,68 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+import com.robut.rirc.IRCConnection;
 import com.robut.rirc.Message;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class Driver {
     public static void main(String[] args) {
+        String userName = "";
+        String auth = "";
+        ArrayList<String> channelList = new ArrayList<>();
+        channelList.add("twitchpresents");
+
         try {
-            tryMsg(":vypur!vypur@vypur.tmi.twitch.tv PRIVMSG #twitchpresents :ATTACK ME AKIZA moon2L");
-            tryMsg("PING tmi.twitch.tv");
+            BufferedReader credFile = new BufferedReader(new FileReader("resources/creds.txt"));
+            userName = credFile.readLine();
+            auth = credFile.readLine();
         }
         catch (Exception e){
             System.err.printf("Exception: %s%n", e);
+            return;
         }
 
-        return;
+        IRCConnection conn = new IRCConnection("irc.chat.twitch.tv", 6667, userName, auth);
+        try {
+            conn.connect();
+            conn.joinChannel("twitchpresents");
+        }
+        catch (IOException e){
+            System.err.printf("Exception connecting: %s%n");
+            return;
+        }
+
+        while (true){
+            try{
+                Message msg = conn.getMessage();
+                printMsg(msg);
+            }
+            catch (Exception e){
+                System.err.printf("Exception getting message: %s%n", e);
+                break;
+            }
+        }
     }
 
     public static void tryMsg(String str) throws Exception{
         Message msg = new Message(str);
-        System.out.printf("Oper: %s%n", msg.getOp());
-        System.out.printf("User: %s%n", msg.getUser());
-        System.out.printf("Chan: %s%n", msg.getChannel());
-        System.out.printf("Mesg: %s%n%n", msg.getMessage());
+        printMsg(msg);
 
         if (msg.getOp().equalsIgnoreCase("PING")) {
             System.out.println("PONG " + msg.getMessage());
         }
+    }
+
+    public static void printMsg(Message msg){
+        System.out.printf("Oper: %s%n", msg.getOp());
+        System.out.printf("User: %s%n", msg.getUser());
+        System.out.printf("Chan: %s%n", msg.getChannel());
+        System.out.printf("Mesg: %s%n%n", msg.getMessage());
     }
 }
--- com/robut/rirc/IRCConnection.java
+++ com/robut/rirc/IRCConnection.java
@@ -36,14 +36,14 @@ public class IRCConnection {
     private BufferedReader sockIn;
     private DataOutputStream sockOut;
 
-    IRCConnection(String serverURL, int serverPort, String userNick, String userAuth){
+    public IRCConnection(String serverURL, int serverPort, String userNick, String userAuth){
         this.serverAddress = serverURL;
         this.port = serverPort;
         this.nick = userNick;
         this.auth = userAuth;
     }
 
-    IRCConnection(String serverURL, int serverPort, String userNick, String userAuth, ArrayList<String> autoChannels){
+    public IRCConnection(String serverURL, int serverPort, String userNick, String userAuth, ArrayList<String> autoChannels){
         this(serverURL, serverPort, userNick, userAuth);
         this.channels.addAll(autoChannels);
     }
@@ -52,18 +52,48 @@ public class IRCConnection {
         this.sock = new Socket(this.serverAddress, this.port);
         this.sockIn = new BufferedReader(new InputStreamReader(this.sock.getInputStream()));
         this.sockOut = new DataOutputStream(this.sock.getOutputStream());
+
+        System.out.printf("Sending auth: %s%n", this.auth);
+        writeMessage("PASS " + this.auth);
+        System.out.printf("Sending nick: %s%n", this.nick);
+        writeMessage("NICK " + this.nick);
+
+        System.out.printf("Finished connecting.%n");
+
+        for (String channel : channels){
+            System.out.printf("Joining channel %s%n", channel);
+            joinChannel(channel);
+        }
     }
 
-    private Message getMessage() throws IOException, RIRCException{
+    public void joinChannel(String channelName) throws IOException{
+        if (!channels.contains(channelName)){
+            channels.add(channelName);
+        }
+        if (this.sock.isConnected()) {
+            writeMessage("JOIN #" + channelName);
+        }
+    }
+
+    public Message getMessage() throws IOException, RIRCException{
         if (sock == null || !sock.isConnected()){
             throw new IOException("Socket isn't connected to a server. Call connect() method first.");
         }
 
-        Message msg = new Message(sockIn.readLine());
+        String rawMsg = sockIn.readLine();
+        Message msg = new Message(rawMsg);
         while (!msg.getOp().equalsIgnoreCase("PRIVMSG")){
+            System.out.printf("Message: %s%n%n", rawMsg);
             msg = new Message(sockIn.readLine());
         }
 
         return msg;
+    }
+
+    public void writeMessage(String msg) throws IOException {
+        if (msg.contains("\r\n")){
+            throw new IOException("Error: Message contains newline characters.");
+        }
+        this.sockOut.write((msg + "\r\n").getBytes("UTF-8"));
     }
 }
